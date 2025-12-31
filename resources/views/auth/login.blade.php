580b0e763e58f455c31f4035d5106325d331b527
rabeyabushritamanna-cyber <rabeyabushritamanna@gmail.com>
2025-12-31 06:35:45 +0600

    some-change

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,7 +4,9 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>Admin Panel</title>
+    
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
     <link rel="stylesheet" href="{{asset("contents/admin")}}/css/bootstrap.min.css">
     <link rel="stylesheet" href="{{asset("contents/admin")}}/css/all.min.css">
     <link rel="stylesheet" href="{{asset("contents/admin")}}/css/style.css">
